b82f70bd67cd56ef6dd1454a57076a94a8043c76
unknown <lipebrabes@gmail.com>
2023-09-14 17:08:26 -0300

    Add: HAPT Processor
    Adding class responsible for processing HAPT dataset, according to the pipeline.

--- scratch/datasets/__init__.py
+++ scratch/datasets/__init__.py
@@ -2,4 +2,5 @@ from .dataset_factory import DatasetFactory
 from .pamap import PAMAP2DataProcessor
 from .ucihar import UCIHARDataProcessor
 from .dsads import DSADSDataProcessor
+from .hapt import HAPTDataProcessor
 from .processing import *
